abe690749222e8372d621265fae0360361ce3120
parihargaurav <parihargaurav527@gmail.com>
2024-05-23 13:18:37 +0530

    class

--- class.cpp
+++ class.cpp
@@ -9,7 +9,7 @@ public:
     int d, e;
     // they are accessible outside the class also
     void setData(int a, int b, int c); // Declaration
-    void getData()
+    void getData()// print data
     {
         cout << a << endl;
         cout << b << endl;
@@ -19,14 +19,15 @@ public:
     }
     // “setData” function is used to assign values to the private member of the class
 };
-void Employee ::setData(int a1, int b1, int c1)
+// private ko usi class ka function hi access kar sakhta hai.. aise public ki tarah access nahi hota
+void Employee ::setData(int a1, int b1, int c1) // :: scope resolution operator
 {
     a = a1;
     b = b1;
     c = c1;
 };
 int main(){
-Employee gaurav;
+Employee gaurav;  // employee gaurav object
 gaurav.d = 90;
 gaurav.e = 9877655;
 gaurav.setData(45,555,666);
@@ -34,3 +35,9 @@ gaurav.getData();
 
 return 0;
 };
+
+
+// typedef is used to change the datatype of any datatype
+
+// difference between structure and classess:-
+// in structure we can't do data hiding while in classess we can do data hiding 
